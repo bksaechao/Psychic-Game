2476da16798ccca50f1be21f06eec1efcc6ccb41
CaoKao <bksaechao@gmail.com>
2019-10-04 02:06:31 -0700

    linked js file and set up starting variables and initialized an alphabet array with all the letters of the alphabet

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+let wins = 0;
+let loss = 0;
+let guessesLeft = 9;
+let currentGuesses = "";
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
