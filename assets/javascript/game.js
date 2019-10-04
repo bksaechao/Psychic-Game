a8293c4cf8e46bcb812aae33dfc88c290ad1f702
CaoKao <bksaechao@gmail.com>
2019-10-04 05:06:22 -0700

    grabbed elements by id in js file and started layout for the onkeyup function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,25 @@
-let wins = 0;
+//variables
+let win = 0;
 let loss = 0;
 let guessesLeft = 9;
-let currentGuesses = "";
+let currentGuesses = [];
 
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//alphabet array
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Grabbing elements
+document.getElementById("w").innerHTML = win;
+document.getElementById("l").innerHTML = loss;
+document.getElementById("gl").innerHTML = guessesLeft;
+document.getElementById("cg").innerHTML = currentGuesses;
+//Computer picks a random letter
+//testing global variable
+var pickLetter = alphabet[Math.floor(Match.random() * alphabet.length)];
+
+document.onkeyup = function (event) {
+    //-1 guess
+    guessesLeft--;
+    gl.innerHTML = guessesLeft;
+    //User Guess
+    var userGuess = event.key;
+}
