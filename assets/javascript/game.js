a60e7b5752edc63b6b780b160ce0c8c8b393b9f3
CaoKao <bksaechao@gmail.com>
2019-10-05 15:00:15 -0700

    created a reset function and set up if statements to compare answers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,31 +6,58 @@ let currentGuesses = [];
 
 //alphabet array
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+// reset function
+function reset() {
+    guessesLeft = 9;
+    currentGuesses = [];
+    gl.innerHTML = guessesLeft;
+    cg.innerHTML = currentGuesses;
+    pickLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(pickLetter)
+}
+//Computer picks random letter
+var pickLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(pickLetter);
 //Grabbing elements
 document.getElementById("w").innerHTML = win;
 document.getElementById("l").innerHTML = loss;
 document.getElementById("gl").innerHTML = guessesLeft;
 document.getElementById("cg").innerHTML = currentGuesses;
-
 document.onkeyup = function (event) {
-    //-1 guess
-    guessesLeft--;
-    gl.innerHTML = guessesLeft;
-    //Computer picks random letter
-    var pickLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-    console.log(pickLetter)
     //User Guess
     var userGuess = event.key;
-    //displays userGuess on Current guesses
-    currentGuesses.push(userGuess);
-    cg.innerHTML = currentGuesses;
+    //Checks to see if userGuess is a value in alphabet
+    var checkAlphabet = alphabet.includes(userGuess);
+    //Push userGuess onto current guesses
+    var checkGuess = currentGuesses.includes(userGuess);
+    //if statements to compare guesses
+    if (checkAlphabet === false) {
+        alert("Not a valid input! Try again!")
+        console.log(userGuess);
+    } else {
+        currentGuesses.push(userGuess);
+        cg.innerHTML = currentGuesses;
+    }
 
 
+    if (checkGuess === true) {
+        alert("You already picked that one! Try again!")
+        currentGuesses.pop();
+    } else if (checkAlphabet === false) {
+    }
+    else {
+        guessesLeft--;
+        gl.innerHTML = guessesLeft;
+    }
 
-
-    //Compare results
-};
-
-
+    if (userGuess === pickLetter) {
+        alert("Wow! You got it! Woot!")
+        win++;
+        w.innerHTML = win;
+        reset();
+    } else if (guessesLeft <= 0) {
+        loss++;
+        l.innerHTML = loss;
+        reset();
+    }
+}
